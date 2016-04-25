035a0e461f59b0827985a271da8380455b339494
Avinash Jagan <Avinash Jagan>
2016-04-24 22:34:33 -0700

    check in from avinash jagan

---
+++ src/main/java/model/NycTaxiSource.java
@@ -0,0 +1,19 @@
+package model;
+
+public class NycTaxiSource {
+String sourceType;
+String sourceLink;
+public NycTaxiSource(){}
+public String getSourceType() {
+	return sourceType;
+}
+public void setSourceType(String sourceType) {
+	this.sourceType = sourceType;
+}
+public String getSourceLink() {
+	return sourceLink;
+}
+public void setSourceLink(String sourceLink) {
+	this.sourceLink = sourceLink;
+}
+}
